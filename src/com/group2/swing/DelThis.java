c3ac5f8a7d8c4dcb265e7f8eedd2851c2387853a
RapRap <RapRap@DESKTOP-SK2C7NS>
2023-10-29 21:32:22 +0800

    del commit

--- src/com/group2/swing/DelThis.java
+++ src/com/group2/swing/DelThis.java
@@ -11,6 +11,6 @@ package com.group2.swing;
 public class DelThis {
 	public static void main(String[]args){
         
-        System.out.println("Pogi ka");
+        System.out.println("Chriss");
     }
 }
