2d66629864dcf049d142cbebeb388f2796d69d1f
Arvy <Arvy@Arvey>
2023-10-29 20:10:12 +0800

    TestCommit

---
+++ src/com/group2/swing/Test_commit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.group2.swing;
+
+/**
+ *
+ * @author Arvy
+ */
+public class Test_commit {
+    public static void main (String [] args)
+    {
+        System.out.println("PRRRRRRRRT!!!!");
+    }
+}
