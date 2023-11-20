e18c2cacd73a145337750b1aab65887a8a081a3c
RapRap <RapRap@DESKTOP-SK2C7NS>
2023-11-21 07:49:28 +0800

    Admin frame

---
+++ src/com/group2/swing/ScrollBar.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.group2.swing;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JScrollBar;
+
+public class ScrollBar extends JScrollBar{
+	
+	public ScrollBar() {
+        setUI(new ModernScrollBarUI());
+        setPreferredSize(new Dimension(5, 5));
+        setBackground(new Color(242, 242, 242));
+        setUnitIncrement(20);
+    }
+}
