a483c3387b2fe25d9e47d28636bac1969f61328d
RapRap <RapRap@DESKTOP-SK2C7NS>
2023-11-21 07:55:32 +0800

    Admin frame

--- src/com/group2/panel/historyTable.java
+++ src/com/group2/panel/historyTable.java
@@ -1,28 +1,62 @@
 package com.group2.panel;
 
+import java.awt.Color;
+
+import com.group2.swing.*;
+
 public class historyTable extends javax.swing.JPanel {
 
 	public historyTable() {
 		initComponents();
+		tbl();
+	}
+	
+	public void tbl()
+	{
+		spHistoryTbl.setVerticalScrollBar(new ScrollBar());
+		spHistoryTbl.getVerticalScrollBar().setBackground(Color.WHITE);
+		spHistoryTbl.getViewport().setBackground(Color.WHITE);
 	}
 
 	@SuppressWarnings("unchecked")
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
+                spHistoryTbl = new javax.swing.JScrollPane();
+                historyTbl = new javax.swing.JTable();
+
+                setMaximumSize(new java.awt.Dimension(800, 1080));
+                setMinimumSize(new java.awt.Dimension(800, 1080));
+                setPreferredSize(new java.awt.Dimension(800, 1080));
+
+                historyTbl.setModel(new javax.swing.table.DefaultTableModel(
+                        new Object [][] {
+                                {null, null, null, null},
+                                {null, null, null, null},
+                                {null, null, null, null},
+                                {null, null, null, null}
+                        },
+                        new String [] {
+                                "Title 1", "Title 2", "Title 3", "Title 4"
+                        }
+                ));
+                spHistoryTbl.setViewportView(historyTbl);
+
                 javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
                 this.setLayout(layout);
                 layout.setHorizontalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 400, Short.MAX_VALUE)
+                        .addComponent(spHistoryTbl, javax.swing.GroupLayout.DEFAULT_SIZE, 800, Short.MAX_VALUE)
                 );
                 layout.setVerticalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 300, Short.MAX_VALUE)
+                        .addComponent(spHistoryTbl, javax.swing.GroupLayout.DEFAULT_SIZE, 1080, Short.MAX_VALUE)
                 );
         }// </editor-fold>//GEN-END:initComponents
 
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JTable historyTbl;
+        private javax.swing.JScrollPane spHistoryTbl;
         // End of variables declaration//GEN-END:variables
 }
